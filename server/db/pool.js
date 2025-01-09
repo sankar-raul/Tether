19fb4ebca7bde7b08d2cf240b3fd4e4377c14b90
sankar-raul <raulsankar99@gmail.com>
2025-01-10 01:04:18 +0530

    fix: set default database port to 3306 if not specified

--- server/db/pool.js
+++ server/db/pool.js
@@ -5,7 +5,7 @@ let pool = createPool({
     host: process.env.HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    port: Number(process.env.DB_PORT) || 3306,
     database: process.env.DB,
     connectionLimit: 10,
     queueLimit: 0
