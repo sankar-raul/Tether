655a1f7709beb32697c46786a302dd2c10bb8e9d
sankar-raul <raulsankar99@gmail.com>
2025-02-02 10:42:20 +0530

    feat: enhance ChatBox component with improved message display and scrolling functionality

--- client/src/components/ChatBox/ChatBox.jsx
+++ client/src/components/ChatBox/ChatBox.jsx
@@ -1,18 +1,21 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import useContacts from '../../context/contacts/contact'
 import styles from './chatbox.module.css'
 import useMsgSocket from '../../hook/useMsgSocket'
-
+import chatNavStyle from './chatNav.module.css'
 const ChatBox = () => {
     const { selectedContact, getContactInfo } = useContacts()
     const [ chatingWith, setChatingWith ] = useState(getContactInfo(selectedContact) || {})
-    const { messages, seenMap, sendMsg, seeMsg } = useMsgSocket(Number(selectedContact))
+    const { messages, sendMsg, seeMsg } = useMsgSocket(Number(selectedContact))
     const [ text, setText ] = useState('')
     const [ chats, setChats ] = useState([])
+    const scrollRef = useRef(null)
+
     const handleInput = (e) => {
         setText(e.target.value)
     }
     const send = () => {
+        scrollRef.current.scrollIntoView()
         const messageContent = text.trim()
         if (messageContent.length > 0) {
             setText('')
@@ -27,7 +30,8 @@ const ChatBox = () => {
         setChats(chat)
         console.log(messages)
         console.log(chat)
-    }, [messages, selectedContact, seeMsg])
+    }, [messages, selectedContact, seeMsg, chatingWith])
+
     useEffect(() => {
         if (!chatingWith || !chatingWith.unread) return
         if (chatingWith.unread > 0) {
@@ -37,16 +41,44 @@ const ChatBox = () => {
     useEffect(() => {
         setChatingWith(getContactInfo(selectedContact) ?? {})
     }, [selectedContact, getContactInfo])
+
+    useEffect(() => {
+        if (!scrollRef.current) return
+        scrollRef.current.scrollIntoView()
+    }, [scrollRef, selectedContact])
     return (
+        <>
+        {selectedContact != null && selectedContact != 0 ?
             <section className={styles['chat-box']}>
-                <h1>Chatting with {chatingWith.username}</h1>
+                <ChatContactHeader />
+                {/* <h1>Chatting with {chatingWith.username}</h1> */}
+              <main className={styles['msgs']}>
+                <div ref={scrollRef} className={styles['scroll']}>scroll</div>
               {
-                chats.map(msg => <h6 key={msg.id} name={msg.id}>{msg.content}</h6>)
+                chats.slice().reverse().map(msg =>
+                <div key={msg.id}>
+                    <span name={msg.id} style={msg.sender == chatingWith.id ? {float: 'left'} : {float: 'right'}}>{msg.content}</span>
+                </div>
+                )
               }
-              <input onChange={handleInput} type="text" value={text} />
-              <button onClick={send}>Send</button>
+              </main>
+              <div className={styles['msg-send-bar']}>
+                <input onChange={handleInput} type="text" value={text} />
+                <button onClick={send}>Send</button>
+              </div>
             </section>
+            : <section>Please select a contact</section>
+}
+        </>
     )
 }
 
+const ChatContactHeader = () => {
+    
+    return (
+        <nav className={chatNavStyle['chat-nav']}>
+header
+        </nav>
+    )
+}
 export default ChatBox
