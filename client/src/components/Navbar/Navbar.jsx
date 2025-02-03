488b5aaff0c24fbf63f7dd7cf5fb79abe6a27a8e
sankar-raul <raulsankar99@gmail.com>
2025-02-03 10:02:17 +0530

    Update Navbar component to use app-dark.png for improved image support

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import styles from './navbar.module.css'
-import appLogo from '../../assets/svg/app-dark.svg'
+import appLogo from '../../assets/images/app-dark.png'
 import useUserInfo from '../../context/userInfo/userInfo'
 import { useEffect } from 'react'
 const Navbar = () => {
