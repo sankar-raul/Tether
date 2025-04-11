3b3aa99234b5e28396b1ad46fb6f5ae0fb93c618
sankar-raul <raulsankar99@gmail.com>
2025-04-11 14:02:08 +0530

    feat: implement dynamic height adjustment for RightContainer on mobile; enhance responsiveness with debounce and throttle

--- client/src/components/RightContainer/RightContainer.jsx
+++ client/src/components/RightContainer/RightContainer.jsx
@@ -5,11 +5,36 @@ import CallBox from "../CallBox/CallBox"
 import ChatBox from "../ChatBox/ChatBox"
 import styles from "./rightcontainer.module.css"
 import useContacts from "../../context/contacts/contact"
+import { useEffect } from "react"
+import { useMediaQuery } from "react-responsive"
+import { debounce, throttle } from '../../utils/helperFunctions'
+
 const RightContainer = () => {
     const { currentTab } = useTabs()
     const { selectedContact } = useContacts()
+    const isMobile = useMediaQuery({ query: '(max-width: 700px)' })
 
-    // console.log(selectedContact)
+    useEffect(() => {
+        if (!isMobile) {
+            return
+        }
+        const setHeight = () => {
+            // console.log(window.innerHeight)
+            document.documentElement.style.setProperty('--window-height', `${window.innerHeight}px`)
+        }
+        const setChatBoxHeightDebounce = debounce(setHeight, 50)
+        const setChatBoxHeightThrottle = throttle(setHeight, 50) // 👍
+
+        const setChatBoxHeight = () => {
+            setChatBoxHeightThrottle()
+            setChatBoxHeightDebounce()
+        }
+        window.addEventListener('resize', setChatBoxHeight)
+        setHeight()
+        return () => {
+            window.removeEventListener('resize', setChatBoxHeightDebounce)
+        }
+    }, [isMobile])
 
     return (
         <MessageProvider>
