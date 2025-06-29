0b0aa095474ea7cc74c15b6bd2114db3214c26d4
Sankar <raulsankar99@gmail.com>
2025-06-29 23:32:52 +0530

    refactor: remove console log from useSwitch effect for cleaner code

--- client/src/hook/Switch/useSwitch.jsx
+++ client/src/hook/Switch/useSwitch.jsx
@@ -9,9 +9,7 @@ export default function useSwitch({isOn=false, acentColor, ...props}={}) {
         setIsEnabled(prev => !prev)
     }, [])
 
-    useEffect(() => {
-        console.log(isEnabled)
-    }, [isEnabled])
+ 
     const SwitchElement = () => <div onClick={handleSwitchClick} {...props} className={`${styles['switch-parent']} ${isEnabled ? ' ' + styles['on'] : ''}`}>
                             <div></div>
                         </div>
