b7f33786b9eb2a6a5025e88944f728be1f60b9d9
Sankar <raulsankar99@gmail.com>
2025-10-26 17:16:47 +0530

    Refactor code structure for improved readability and maintainability

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,18 @@
 // import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { registerSW } from 'virtual:pwa-register'
 import './index.css'
 import App from './App.jsx'
 
 const root = createRoot(document.getElementById('root'))
 root.render(<App />)
 
-// let timeout = null
-// document.documentElement.onclick = (e) => {
-//   e.target.style.cursor = "url('/svg/cursor-clicked.svg') 10 10, auto"
-//   timeout && clearTimeout(timeout)
-//   timeout = setTimeout(() => {
-//     e.target.style.cursor = "url('/svg/cursor.svg') 10 10, auto"
-//   }, 100)
-// }
+const updateSW = registerSW({
+  onNeedRefresh() {
+    // immediately reload the page to get new content
+    updateSW(true);
+  },
+  onOfflineReady() {
+    console.log('App ready to work offline');
+  }
+});
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import fs from 'fs'
+import { VitePWA } from 'vite-plugin-pwa'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'ChessGeek',
+        short_name: 'ChessGeek',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#172430',
+        theme_color: '#172430',
+        icons: [
+          { src: 'logo-rounded.png', sizes: '192x192', type: 'image/png' },
+          { src: 'logo-rounded.png', sizes: '512x512', type: 'image/png' }
+        ]
+      }
+    })
+  ],
   build: {
     outDir: 'dist'
   },
+  
   server: {
     historyApiFallback: true,
     https: {
@@ -15,4 +35,5 @@ export default defineConfig({
     },
     host: '0.0.0.0'
   },
+  
 })
